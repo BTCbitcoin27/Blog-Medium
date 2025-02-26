a974aff0b074ff0242028ef764078c23c8883ee2
BTCbitcin27 <androxus4ever@gmail.com>
2025-02-25 20:58:42 -0600

    Add project files.

---
+++ Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,33 @@
+﻿using BlogCoreDataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContainerWork _containerWork;
+        public ArticulosController(IContainerWork containerWork)
+        {
+            _containerWork = containerWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _containerWork.Articulo.GetAll(includeProperties: "Categoria") });
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,89 @@
+﻿using BlogCore.Models;
+using BlogCoreDataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContainerWork _containerWork;
+        public CategoriasController(IContainerWork containerWork)
+        {
+            _containerWork = containerWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _containerWork.Categoria.Add(categoria);
+                _containerWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria = _containerWork.Categoria.Get(id);
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _containerWork.Categoria.Update(categoria);
+                _containerWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _containerWork.Categoria.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDB = _containerWork.Categoria.Get(id);
+            if (objFromDB == null)
+            {
+                return Json(new { success = false, message = "Error borrando categoria" });
+            }
+
+            _containerWork.Categoria.Remove(objFromDB);
+            _containerWork.Save();
+            return Json(new { success = true, message = "Borrando categoria" });
+
+        }
+
+
+    }
+}
