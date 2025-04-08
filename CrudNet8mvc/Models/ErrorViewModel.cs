53d54fec25dadf9575b423073fb326ea346a2ee0
BTCbitcin27 <androxus4ever@gmail.com>
2025-04-07 18:20:16 -0600

    deleted extra files

--- CrudNet8mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrudNet8mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
